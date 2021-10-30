acd0b294e19848f7eca157b3692bac9a97fa323b
Jessy <jessy.etokabeka@gmail.com>
2021-10-30 18:30:18 +0200

    complete modal window

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   // votre code ici...
   console.log('Hello');
+
+  document.getElementById('signup').addEventListener('click', () => {
+    document.getElementById('form-modal').classList.remove('hidden');
+    document.getElementById('body-id').classList.add('hidde-scrollbar');
+  });
+
+  document.getElementById('modal-escape').addEventListener('click', () => {
+    document.getElementById('form-modal').classList.add('hidden');
+    document.getElementById('body-id').classList.remove('hidde-scrollbar');
+  });
+
+  document
+    .getElementById('contactForm')
+    .addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const data = {
+        firstname: document.getElementById('firstname').value,
+        lastname: document.getElementById('lastname').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+      };
+
+      document.getElementById('contactForm').reset();
+
+      await axios.post('http://localhost:3000/form', data);
+    });
 });
